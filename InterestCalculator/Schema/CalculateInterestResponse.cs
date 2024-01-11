aec0f5a7d6691bf95f8f3d9c7c69096fa0b32f2f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 03:17:15 +0300

    commit

--- InterestCalculator/Schema/CalculateInterestResponse.cs
+++ InterestCalculator/Schema/CalculateInterestResponse.cs
@@ -1,6 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace InterestCalculator.Schema;
 
 public class CalculateInterestResponse
 {
-    
+    public double Anapara { get; set; }
+
+    [JsonPropertyName("Faiz Tutarı")] public double FaizTutari { get; set; }
+
+    [JsonPropertyName("Getiri Oranı")] public double GetiriOrani { get; set; }
+
+    [JsonPropertyName("Vade Sonu Toplam")] public double VadeSonuToplam { get; set; }
+
+    public override string ToString()
+    {
+        return $"Anapara: {Anapara:F2},\n" +
+               $"Faiz Tutarı: {FaizTutari:F2},\n" +
+               $"Getiri Oranı: %{GetiriOrani:F2},\n" +
+               $"Vade Sonu Toplam: {VadeSonuToplam:F2}";
+    }
 }

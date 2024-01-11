0c01bec771df66762c3d7bff3b321c4e63af7e95
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-11 03:17:15 +0300

    commit

--- InterestCalculator/Schema/CalculateInterestRequest.cs
+++ InterestCalculator/Schema/CalculateInterestRequest.cs
@@ -1,3 +1,12 @@
 namespace InterestCalculator.Schema;
 
-public record CalculateInterestRequest();
+public record CalculateInterestRequest(
+
+    string Yon,// "S" or "C" compound or simple
+    int Faizlendirme,// 0, 1, 3, 6, 12
+    int FaizBirim,// 0, 1, 12
+    decimal FaizYuzde,// e.g., 10.75
+    decimal Anapara,// e.g., 12500.50
+    int Vade,// e.g., 15
+    int VadeBirim// 0, 1, 12
+);
